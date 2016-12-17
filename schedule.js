cb64b4db3f161b314aaae2b8fc86fe5d4794cda4
azure (on NAS) <spamazure@mail.ru>
2016-12-18 00:52:38 +0200

    log interval increased to 5 min, updated TODO

--- schedule.js
+++ schedule.js
@@ -1,12 +1,7 @@
 var crontab = require('node-crontab');
 var LogJob = require('./log-job');
 
-if (require.main === module) {
-	// called directly
-	crontab.scheduleJob('* * * * *', LogJob);
-} else {
-	module.exports = function() {
-		//save values to db every 5 minutes
-		crontab.scheduleJob('* * * * *', LogJob);
-	};
-}
+module.exports = function() {
+	//save values to db every 5 minutes
+	crontab.scheduleJob('*/5 * * * *', LogJob);
+};
