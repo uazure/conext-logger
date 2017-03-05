72211b30de96e4a647b0fedb9935d993b4e56609
azure (on NAS) <spamazure@mail.ru>
2017-03-05 22:55:22 +0200

    fix code issues

--- app/manager/month-stat-manager.js
+++ app/manager/month-stat-manager.js
@@ -16,6 +16,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+'use strict';
+
 let measurement = require('../model/measurement-model');
 let daySummaryModel = require('../model/day-summary-model');
 let arrayConverter = require('../day-summary-model-array-converter');
